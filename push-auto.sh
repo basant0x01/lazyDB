72335453937b43e5443811470df52a5fc4e1736b
basant0x01 <basantkarki75@gmail.com>
2024-10-30 17:58:15 +0545

    Updated new features on v0.1 2024-10-30 17:58:15

--- push-auto.sh
+++ push-auto.sh
@@ -10,17 +10,21 @@ if [[ -n $(git status --porcelain) ]]; then
     # Commit changes with a timestamp
     git commit -m "Updated new features on v0.1 $(date +"%Y-%m-%d %H:%M:%S")"
 
-    # Push changes to the master branch
-    # Read the GitHub token from an environment variable
-    if [ -z "$GITHUB_TOKEN" ]; then
-        echo "Error: GITHUB_TOKEN environment variable is not set."
+    # Pull changes from the remote repository
+    if git pull origin master; then
+        # Push changes to the master branch
+        if [ -z "$GITHUB_TOKEN" ]; then
+            echo "Error: GITHUB_TOKEN environment variable is not set."
+            exit 1
+        fi
+
+        # Use the token to push changes
+        git push https://$GITHUB_TOKEN@github.com/basant0x01/lazyDB.git master
+        echo "Changes pushed to GitHub."
+    else
+        echo "Failed to pull changes. Resolve conflicts and try again."
         exit 1
     fi
-
-    # Use the token to push changes
-    git push https://$GITHUB_TOKEN@github.com/basant0x01/lazyDB.git master
-
-    echo "Changes pushed to GitHub."
 else
     echo "No changes to commit."
 fi
