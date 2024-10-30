91274afd4372351b43881cac84fbd218af95e918
basant0x01 <basantkarki75@gmail.com>
2024-10-30 17:59:32 +0545

    Updated new features on v0.1 2024-10-30 17:59:32

--- push-auto.sh
+++ push-auto.sh
@@ -12,13 +12,13 @@ if [[ -n $(git status --porcelain) ]]; then
 
     # Pull changes from the remote repository
     if git pull origin master; then
-        # Push changes to the master branch
+        # Use the token to push changes
         if [ -z "$GITHUB_TOKEN" ]; then
             echo "Error: GITHUB_TOKEN environment variable is not set."
             exit 1
         fi
 
-        # Use the token to push changes
+        # Push changes
         git push https://$GITHUB_TOKEN@github.com/basant0x01/lazyDB.git master
         echo "Changes pushed to GitHub."
     else
